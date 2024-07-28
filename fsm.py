f539fd437898c500b780b5febca69a8f0702ec6f
Samsu-F <samsu-f@web.de>
2024-07-28 16:08:48 +0200

    improve parser and add optional arg to define alphabet

--- fsm.py
+++ fsm.py
@@ -2,10 +2,13 @@
 
 import argparse
 import os
+import sys
 from graphviz import Source
 
 
 UNDEFINED = "_undefined_"
+WILDCARD_NOTATION = "*" # whats written in the fsm file as a wildcard
+WILDCARD = 42 # non-string value without meaning. This is used in the hash maps
 ARGPARSE_EPILOG = """
 Author & License
   Written by Samsu-F, 2024.
@@ -16,13 +19,13 @@ Author & License
 
 
 class Fsm:
-    def __init__(self, fsmfilename):
+    def __init__(self, fsmfilename, alphabet=None):
         self.states = {}
         self.state_accepting = {}
         self.initial_state = None
         with open(fsmfilename, "r") as file:
             lines = file.read().splitlines()
-            for line in lines:
+            for line_number, line in enumerate(lines):
                 line = line.split("//")[0]  # remove comments
                 line = ''.join(c for c in line if c not in " \t")   # remove tabs and spaces
                 if line == '':
@@ -38,6 +41,8 @@ class Fsm:
                 self.states[state_name] = {}
                 self.state_accepting[state_name] = accepting
                 transitions = transitions.split(";")
+                defined_chars = set()
+                wildcard_used = False
                 for t in transitions:
                     if t == '':
                         continue
@@ -46,7 +51,24 @@ class Fsm:
                     destination_state = split[1]
                     char_list = split[0].split(",")
                     for c in char_list:
-                        self.states[state_name][c] = destination_state
+                        if wildcard_used:
+                            sys.exit(f"Invalid fsmfile {fsmfilename}: transitions defined after wildcard transition in line {line_number+1}.")
+                        if c == WILDCARD_NOTATION:
+                            # if wildcard_used:
+                            #     sys.exit(f"Invalid fsmfile {fsmfilename}: multiple wildcards in line {line_number}.")
+                            wildcard_used = True
+                            if alphabet is None:
+                                self.states[state_name][WILDCARD] = destination_state
+                            else:
+                                for c in alphabet:
+                                    if c not in defined_chars:
+                                        self.states[state_name][c] = destination_state
+                                        defined_chars.add(c)
+                        else:
+                            if c in defined_chars:
+                                sys.exit(f"Invalid fsmfile {fsmfilename}: multiple transitions defined for character '{c}' in line {line_number+1}.")
+                            self.states[state_name][c] = destination_state
+                            defined_chars.add(c)
         self.current_state = self.initial_state
 
 
@@ -55,6 +77,9 @@ class Fsm:
         transitions = self.states[self.current_state]
         if input_char in transitions:
             self.current_state = transitions[input_char]
+            return
+        elif WILDCARD in transitions:
+            self.current_state = transitions[WILDCARD]
             return
         else:
             self.current_state = UNDEFINED
@@ -85,6 +110,8 @@ class Fsm:
             if state == UNDEFINED:
                 continue
             for char, next_state in self.states[state].items():
+                if char == WILDCARD:
+                    char = WILDCARD_NOTATION
                 result += f"\t{state} -> {next_state} [label=\"{char}\"]\n"
         result += "}"
         return result
@@ -114,12 +141,15 @@ def parse_args() -> argparse.ArgumentParser:
                                 help="Display the initial state of the fsm.")
     argparser.add_argument('-D', '--display_final', required=False, dest='display_final', default=False, action='store_true',
                                 help="Display the final state of the fsm, final state will be marked in red.")
+    argparser.add_argument('-a', '--alphabet', required=False, type=str, default=None,
+                                help="""Define the alphabet, i.e. the meaning of the wildcard as a comma-separated list.
+                                        For example, -a 'a, b, c'.""")
 
     args = argparser.parse_args()
     if args.display_final and args.inputstring is None:
         argparser.error('-i/--inputstring is mandatory when -D/--display_initial is used')
     if not args.display_initial and not args.display_final and args.inputstring is None:
-        argparser.error('At least one of the options has to be used. Use option -h for help.')
+        argparser.error('At least one of the options -i, -d or -D has to be used. Use option -h for help.')
     return args
 
 
@@ -127,7 +157,10 @@ def parse_args() -> argparse.ArgumentParser:
 
 def main():
     args = parse_args()
-    fsm = Fsm(args.fsmfile)
+    if args.alphabet is not None:
+        args.alphabet = set(args.alphabet.split(','))
+    print(f"{args.alphabet=}")
+    fsm = Fsm(args.fsmfile, alphabet=args.alphabet)
     filename = os.path.basename(args.fsmfile)
     fsmname = filename.rsplit(".fsm", 1)[0]     # remove suffix ".fsm" if it exists
 
